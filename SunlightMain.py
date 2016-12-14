92d24e935a2cb95513a3082fa2d0f26bcb8897cb
Nathan Gilbert <nathan.gilbert@gmail.com>
2016-12-13 20:17:01 -0700

    adding unit test skeleton and bill tracker shell

--- SunlightMain.py
+++ SunlightMain.py
@@ -4,6 +4,15 @@
 import argparse
 import sunlight
 
+#TODO
+def grab_current_house_bills():
+    """ house bills """
+    pass
+
+def grab_current_senate_bills():
+    """ senate bills """
+    pass
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
@@ -14,15 +23,19 @@ if __name__ == "__main__":
         key = keyFile.readline().strip()
 
     sunlight.config.API_KEY = key
-    ut_legs = sunlight.openstates.legislators(state='ut')
+    ut_metadata = sunlight.openstates.state_metadata(state='ut')
+    ut_legislators = sunlight.openstates.legislators(state='ut')
+    ut_bills = sunlight.openstates.bills(state='ut')
 
-    total_leg = 0
-    for leg in ut_legs:
-        print("-"*50)
-        if leg["active"]:
-            total_leg += 1
-            for key in leg:
-                print(key + " " + str(leg[key]))
+    print(ut_bills)
 
-    print("-"*50)
-    print("Total Legislators: " + str(total_leg))
+    # total_leg = 0
+    # for leg in ut_legs:
+    #     print("-"*50)
+    #     if leg["active"]:
+    #         total_leg += 1
+    #         for key in leg:
+    #             print(key + " " + str(leg[key]))
+
+    # print("-"*50)
+    # print("Total Legislators: " + str(total_leg))
---
+++ unittests.py
@@ -0,0 +1,13 @@
+#Author: Nathan Alan Gilbert
+#Last Modified: Tuesday December 13 8:00:08 PM MST 2016
+""" Unit test class """
+import unittest
+
+#TODO
+#https://docs.python.org/2/library/unittest.html
+class TestSunlightClasses(unittest.TestCase):
+    """Class to test sunlight classes"""
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
