a0d5cfc948db0998cf4955030eda0c6ab84a418d
Nathan Gilbert <nathan.gilbert@gmail.com>
2018-03-24 18:17:00 -0600

    working script again

--- UtahLegislatureInfo.py
+++ UtahLegislatureInfo.py
@@ -1,36 +1,50 @@
 """ Main class for handling sunlight API calls """
 import argparse
-import sunlight
 import json
+import time
+import sunlight
 
-def current_house_bills():
+def current_house_bills(year):
     """ house bills """
-    house_bills = {}
-    for bill in ut_bills:
-        if bill["chamber"] == "lower":
-            bill_info = sunlight.openstates.bill_detail(state='ut',
-                    session='2017', bill_id=bill["bill_id"],
-                    chamber=bill['chamber']) house_bills[bill["bill_id"]] =
-                    bill_info
-    return house_bills
+    hb = {}
+    for b in ut_bills:
+        if b["chamber"] == "lower" and b["session"] == year:
+            bill_info = sunlight.openstates.bill_detail(
+                state='ut',
+                session=b["session"],
+                bill_id=b["bill_id"],
+                chamber=b['chamber'])
+            hb[b["bill_id"]] = bill_info
+    return hb
 
-def current_senate_bills():
+def current_senate_bills(year):
     """ senate bills """
-
-    senate_bills = {}
-    for bill in ut_bills:
-        if bill["chamber"] == "upper":
-            bill_info = sunlight.openstates.bill_detail(state='ut',
-                    session='2017', bill_id=bill["bill_id"],
-                    chamber=bill['chamber']) senate_bills[bill["bill_id"]] =
-                    bill_info
-    return senate_bills
+    sb = {}
+    for b in ut_bills:
+        if b["chamber"] == "upper" and b["session"] == year:
+            bill_info = sunlight.openstates.bill_detail(
+                state='ut',
+                session=b["session"],
+                bill_id=b["bill_id"],
+                chamber=b['chamber'])
+            sb[b["bill_id"]] = bill_info
+    return sb
 
 if __name__ == "__main__":
+    start_time = time.time()
+    this_year = '2018'
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--verbose", action="store_true", help="increase
-            output verbosity") parser.add_argument("-c", "--config", type=str,
-            help="increase output verbosity", default="config.json")
+    parser.add_argument("-v",
+                        "--verbose",
+                        action="store_true",
+                        help="increase output verbosity"
+                       )
+    parser.add_argument("-c",
+                        "--config",
+                        type=str,
+                        help="increase output verbosity",
+                        default="config.json"
+                       )
     args = parser.parse_args()
 
     config = None
@@ -49,8 +63,10 @@ if __name__ == "__main__":
     print("UT Legislators: " + str(len(ut_legislators)))
     print("UT Bills: " + str(len(ut_bills)))
 
-    house_bills = current_house_bills()
-    senate_bills = current_senate_bills()
-
-    for bill in house_bills:
-        print(bill)
+    house_bills = current_house_bills(this_year)
+    print("Number of House Bills: " + str(len(house_bills)))
+    senate_bills = current_senate_bills(this_year)
+    print("Number of Senate Bills: " + str(len(senate_bills)))
+    #for bill in house_bills:
+    #    print(bill)
+    print("--- {0} seconds ---" % (time.time() - start_time))
