0703cea6c2d5f0691ffb969a47f2240f09c3ee1e
Nathan Gilbert <nathan.gilbert@gmail.com>
2020-06-08 18:47:21 -0600

    rename files, add more queries

--- ut-leg-service/src/main/kotlin/org/mediumcool/integrations/openstate/person/OpenStatesPersonAPIImpl.kt
+++ ut-leg-service/src/main/kotlin/org/mediumcool/integrations/openstate/person/OpenStatesPersonAPIImpl.kt
@@ -4,14 +4,14 @@ import io.ktor.util.KtorExperimentalAPI
 import kotlinx.coroutines.runBlocking
 import org.mediumcool.integrations.common.OpenStatesService
 import org.mediumcool.person.domain.PersonDto
-import org.mediumcool.graphql.generated.UtahLegislators
+import org.mediumcool.graphql.generated.UtahGovernment
 import org.springframework.stereotype.Service
 
 @Service
 class OpenStatesPersonAPIImpl: OpenStatesPersonAPI, OpenStatesService() {
   @KtorExperimentalAPI
   override fun getByBody(body: String): List<PersonDto> {
-    val utahLegislators = UtahLegislators(client)
+    val utahLegislators = UtahGovernment(client)
     val orgNames = mutableListOf<PersonDto>()
     runBlocking {
       val results = utahLegislators.execute()
